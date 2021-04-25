8ff850a57fccc04602b3c0dcfb222690c02c85f7
WoMspace <alex0002b@gmail.com>
2021-04-25 20:13:50 +0100

    final mac compat change I think

--- shaders/lib/fog.glsl
+++ shaders/lib/fog.glsl
@@ -7,24 +7,26 @@ vec3 doFog(float depth, vec3 color)
 	vec3 customFogColor;
 	float fogNearValue;
 	float fogFarValue;
-	switch(isEyeInWater)
+
+	if(isEyeInWater == 0) //air
 	{
-		case 0: //air
-			customFogColor = fogColor;
-			fogNearValue = FOG_NEAR;
-			fogFarValue = FOG_END;
-			break;
-		case 1: //water
-			customFogColor = vec3(WATER_FOG_R, WATER_FOG_G, WATER_FOG_B);
-			fogNearValue = 0.0;
-			fogFarValue = WATER_FOG_DISTANCE;
-			break;
-		case 2: //lava
-			customFogColor = vec3(LAVA_FOG_R, LAVA_FOG_G, LAVA_FOG_B);
-			fogNearValue = 0.0;
-			fogFarValue = LAVA_FOG_DISTANCE;
-			break;
+		customFogColor = fogColor;
+		fogNearValue = FOG_NEAR;
+		fogFarValue = FOG_END;
 	}
+	else if(isEyeInWater == 1) //water
+	{
+		customFogColor = vec3(WATER_FOG_R, WATER_FOG_G, WATER_FOG_B);
+		fogNearValue = 0.0;
+		fogFarValue = WATER_FOG_DISTANCE;
+	}
+	else if(isEyeInWater == 2) //lava
+	{
+		customFogColor = vec3(LAVA_FOG_R, LAVA_FOG_G, LAVA_FOG_B);
+		fogNearValue = 0.0;
+		fogFarValue = LAVA_FOG_DISTANCE;
+	}
+	
 	if(texture2D(depthtex0, texcoord).r != 1.0)
 	{
 		color = mix(color, customFogColor, clamp(((length(viewPos)-fogNearValue)/fogFarValue), 0.0, 1.0));
